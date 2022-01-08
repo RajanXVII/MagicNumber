9aff3b3476eb598e3546fae6bd75df1b18b912aa
Rajan Sonvane <44581384+RajanXVII@users.noreply.github.com>
2022-01-08 18:55:59 +0530

    Create magicNumber.java

---
+++ magicNumber.java
@@ -0,0 +1,34 @@
+class palindorme{
+public static void main(String[] args){
+    int number=99999991;
+    int total=0;
+    do{
+        total=calculate(number);
+        number=total;
+        System.out.println("Inside do "+total);
+    }while(total>9);
+    if(total==1){
+        System.out.println("Magic Number!!");
+    }
+    
+}
+private static int calculate(int number){
+        
+    int num2=0;
+    int total=0;
+    while(number>0){
+    num2=getRemainder(number);//3
+    System.out.println("Remainder:"+num2);
+    number=updateNumber(number,num2);
+    total=total+num2;
+    }
+    return total;
+    }
+    private static int getRemainder(int number){
+        return number%10;
+    }
+    
+    private static int updateNumber(int number,int num2){
+        return (number-num2)/10;
+    }
+}
